305d03b25e637d4e914aaab7bb883bcffec4d9a6
Matt Dubie <mbdubie@gmail.com>
2016-07-27 15:43:17 -0700

    wbpck

--- dist/bundle.js
+++ dist/bundle.js
@@ -81827,24 +81827,7 @@
 /* 851 */
 /***/ function(module, exports) {
 
-	module.exports = {
-	  prioritizeTasks
-	};
-	
-	/**
-	 * @typedef PrioritizedTasks
-	 * @type {object}
-	 * @property {Task[]} recent An array of recently completed tasks
-	 * @property {Task[]} urgent An array of urgent tasks
-	 * @property {Task[]} overdue An array of overdue tasks
-	 */
-	
-	/**
-	 * Prioritizes tasks according to their due date and interval length
-	 * @param  {object[]} allTasks      An array of all tasks
-	 * @return {PrioritizedTasks}       An object grouping tasks by priority
-	 */
-	function prioritizeTasks(allTasks) {
+	module.exports.prioritizeTasks = function prioritizeTasks(allTasks) {
 	  let tasks = { recent: [], urgent: [], overdue: [] };
 	  let now = Date.now();
 	
@@ -81861,7 +81844,8 @@
 	  });
 	
 	  return tasks;
-	}
+	};
+	
 
 
 /***/ }
