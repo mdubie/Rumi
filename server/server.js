d87dc7d27f84452710a652124629dde98dbbd26f
Matt Dubie <mbdubie@gmail.com>
2016-07-27 15:51:57 -0700

    disabled authentication

--- server/server.js
+++ server/server.js
@@ -4,30 +4,30 @@ let bodyParser = require('body-parser');
 let morgan = require('morgan');
 
 let decorate = require('./alphadeltaninerniner.service');
-let auth = require('./auth');
+// let auth = require('./auth');
 
 // checkForEnvironmentVariables(['FB_ID', 'FB_SECRET', 'SESSION_SECRET']);
 
-let sessionMiddleware = session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-});
+// let sessionMiddleware = session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false
+// });
 
 // middleware configuration
 let app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(sessionMiddleware);
-app.use(auth.passport.initialize());
-app.use(auth.passport.session());
-app.use(auth.routes);
-app.use(express.static(__dirname + '/../public'));
-app.use(auth.isAuth, express.static(__dirname + '/../dist'));
-let server = decorate(app, sessionMiddleware);
+// app.use(sessionMiddleware);
+// app.use(auth.passport.initialize());
+// app.use(auth.passport.session());
+// app.use(auth.routes);
+// app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/../dist'));
+// let server = decorate(app, sessionMiddleware);
 
 
-console.log('process.env.DATABASE_URL ' , process.env.DATABASE_URL);
+// console.log('process.env.DATABASE_URL ' , process.env.DATABASE_URL);
 
 module.exports = server;
 
