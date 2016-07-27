fdde19ce6565238aff25217afd642f8c597f365f
Matt Dubie <mbdubie@gmail.com>
2016-07-27 14:58:42 -0700

    removed budle.js from gitignore

--- server/env.js
+++ server/env.js
@@ -1,7 +1,7 @@
 module.exports = {
   sessionSecret: process.env.SESSION_SECRET || 'secret',
   serverPort: process.env.PORT || 3000,
-  facebookID: process.env.FB_ID || '626198337538519',
-  facebookSecret: process.env.FB_SECRET || 'd971a4e38c255d4e985ae758cc714b28',
   databaseURL: process.env.DATABASE_URL || 'postgres://mdubie:ttam3199@mydbinstance.c26d9oujglql.us-west-1.rds.amazonaws.com:5432/newrumi',
 };
+
+console.log('process.env.DATABASE_URL ' , process.env.DATABASE_URL);
